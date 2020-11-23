0de50511b6d6e99491b2f3b9d1e2f3dc2f6c661b
kuma <inataku0121@gmail,com>
2020-11-23 09:12:26 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// var shoppinglist = new Vue({
+//   el:"#shopping-list",
+//   data: {
+//     header:"shopping list app",
+//     items: [
+//       "10 party hats",
+//       "2 bord games",
+//       "20 cups",
+//     ]
+//   }
+// });
